b0e1e757c46f4cffa0df5161bfbab2775439a011
Mikita Kobyzau <Mikita_Kobyzau@epam.com>
2021-04-24 18:20:52 +0300

    8.0 Feedback

--- src/main/java/by/kobyzau/tg/bot/pbot/handlers/stat/impl/FeedbackStatHandler.java
+++ src/main/java/by/kobyzau/tg/bot/pbot/handlers/stat/impl/FeedbackStatHandler.java
@@ -30,28 +30,12 @@ public class FeedbackStatHandler implements StatHandler {
 
   @Override
   public void printStat(long chatId) {
-    List<Long> chatIds = telegramService.getChatIds();
-    botActionCollector.text(chatId, new ParametizedText("{0} чатов", new IntText(chatIds.size())));
-    for (long id : chatIds) {
-      printChatInfo(chatId, id);
-    }
-  }
-
-  private void printChatInfo(long sendToChatId, long chatId) {
+    botActionCollector.wait(chatId, 1, ChatAction.TYPING);
     List<Feedback> feedbacks =
         feedbackService.getFeedbacks().stream()
-            .filter(f -> f.getChatId() == chatId)
             .filter(f -> f.getFeedbackType() == FeedbackType.VERSION)
             .collect(Collectors.toList());
     TextBuilder tx = new TextBuilder();
-    tx.append(new SimpleText("Chat ID " + chatId)).append(new NewLineText());
-    Optional<Chat> chat = telegramService.getChat(chatId);
-    chat.ifPresent(
-        c ->
-            tx.append(new SimpleText("Chat " + chatId))
-                .append(new SimpleText(" "))
-                .append(getChatName(c))
-                .append(new NewLineText()));
     for (FeedbackEmojiType emojiType : FeedbackEmojiType.values()) {
       tx.append(new NewLineText());
       tx.append(new SimpleText(emojiType.getEmoji()))
@@ -60,17 +44,7 @@ public class FeedbackStatHandler implements StatHandler {
               new LongText(feedbacks.stream().filter(f -> f.getEmojiType() == emojiType).count()));
     }
 
-    botActionCollector.wait(sendToChatId, 1, ChatAction.TYPING);
-    botActionCollector.text(sendToChatId, tx);
-  }
-
-  private Text getChatName(Chat chat) {
-    return Optional.ofNullable(chat)
-        .map(Chat::getTitle)
-        .filter(StringUtil::isNotBlank)
-        .map(TGUtil::escapeHTML)
-        .map(SimpleText::new)
-        .orElse(new SimpleText("..."));
+    botActionCollector.text(chatId, tx);
   }
 
   @Override
