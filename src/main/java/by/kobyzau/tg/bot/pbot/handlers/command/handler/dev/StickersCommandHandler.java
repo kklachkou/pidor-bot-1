bc7f2d20ac7e35972407d52527bbd58d514691d2
Mikita Kobyzau <Mikita_Kobyzau@epam.com>
2021-08-13 16:45:35 +0300

    11.1 Limits Improve StickersCommandHandler

--- src/main/java/by/kobyzau/tg/bot/pbot/handlers/command/handler/dev/StickersCommandHandler.java
+++ src/main/java/by/kobyzau/tg/bot/pbot/handlers/command/handler/dev/StickersCommandHandler.java
@@ -3,6 +3,7 @@ package by.kobyzau.tg.bot.pbot.handlers.command.handler.dev;
 import by.kobyzau.tg.bot.pbot.collectors.BotActionCollector;
 import by.kobyzau.tg.bot.pbot.handlers.command.Command;
 import by.kobyzau.tg.bot.pbot.handlers.command.handler.CommandHandler;
+import by.kobyzau.tg.bot.pbot.program.text.SimpleText;
 import by.kobyzau.tg.bot.pbot.tg.action.SendStickerBotAction;
 import by.kobyzau.tg.bot.pbot.tg.sticker.StickerType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 import java.util.Optional;
+import org.telegram.telegrambots.meta.api.objects.stickers.Sticker;
 
 @Component
 @Profile("dev")
@@ -22,6 +24,12 @@ public class StickersCommandHandler implements CommandHandler {
   @Override
   public void processCommand(Message message, String text) {
     long chatId = message.getChatId();
+    Message replyToMessage = message.getReplyToMessage();
+    if (replyToMessage != null && replyToMessage.hasSticker()) {
+      Sticker sticker = replyToMessage.getSticker();
+      botActionCollector.text(chatId, new SimpleText(sticker.getFileId()), replyToMessage.getMessageId());
+      return;
+    }
     Optional<StickerType> sticker = StickerType.parseSticker(text);
     if (sticker.isPresent()) {
       for (String stickerId : sticker.get().getStickers()) {
