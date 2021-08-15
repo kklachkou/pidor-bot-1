8616f1be252330a38b4299265abbb77155fe92d6
Mikita Kobyzau <Mikita_Kobyzau@epam.com>
2021-08-15 20:18:11 +0300

    11.2.2 Hot Potato Added questionnaire

--- src/main/java/by/kobyzau/tg/bot/pbot/repository/questionnaire/DBQuestionnaireAnswerRepository.java
+++ src/main/java/by/kobyzau/tg/bot/pbot/repository/questionnaire/DBQuestionnaireAnswerRepository.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Component
-@Profile("prod")
+//@Component
+//@Profile("prod")
 public class DBQuestionnaireAnswerRepository implements QuestionnaireAnswerRepository {
   @Autowired private IQuestionnaireAnswerRepository repository;
 
