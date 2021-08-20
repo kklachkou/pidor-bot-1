1c2297f2df4e2cecfe56d304636d85c9f2972e95
Mikita Kobyzau <Mikita_Kobyzau@epam.com>
2021-08-20 18:08:44 +0300

    11.3.3 Long Live Belarus Removed Name

--- src/main/java/by/kobyzau/tg/bot/pbot/handlers/command/handler/dev/TestCommandHandler.java
+++ src/main/java/by/kobyzau/tg/bot/pbot/handlers/command/handler/dev/TestCommandHandler.java
@@ -3,71 +3,34 @@ package by.kobyzau.tg.bot.pbot.handlers.command.handler.dev;
 import by.kobyzau.tg.bot.pbot.collectors.BotActionCollector;
 import by.kobyzau.tg.bot.pbot.handlers.command.Command;
 import by.kobyzau.tg.bot.pbot.handlers.command.handler.CommandHandler;
-import by.kobyzau.tg.bot.pbot.model.dto.AlertDto;
-import by.kobyzau.tg.bot.pbot.tg.action.SimpleBotAction;
-import by.kobyzau.tg.bot.pbot.util.StringUtil;
-import java.util.Collections;
-import java.util.Random;
+import by.kobyzau.tg.bot.pbot.model.Pidor;
+import by.kobyzau.tg.bot.pbot.program.text.SimpleText;
+import by.kobyzau.tg.bot.pbot.repository.pidor.PidorRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 @Component
 public class TestCommandHandler implements CommandHandler {
+
+  @Autowired private PidorRepository pidorRepository;
 
   @Autowired private BotActionCollector botActionCollector;
 
   @Override
   public void processCommand(Message message, String text) {
-    botActionCollector.add(
-        new SimpleBotAction<>(
-            message.getChatId(),
-            SendMessage.builder()
-                .text("Test 1")
-                .chatId(message.getChatId().toString())
-                .replyMarkup(
-                    InlineKeyboardMarkup.builder()
-                        .keyboardRow(
-                            Collections.singletonList(
-                                InlineKeyboardButton.builder()
-                                    .text("Alert")
-                                    .callbackData(
-                                        StringUtil.serialize(new AlertDto("Alert", true, null)))
-                                    .build()))
-                        .keyboardRow(
-                            Collections.singletonList(
-                                InlineKeyboardButton.builder()
-                                    .text("Alert Cached")
-                                    .callbackData(
-                                        StringUtil.serialize(
-                                            new AlertDto(
-                                                "Alert " + (new Random().nextInt() % 100),
-                                                true,
-                                                10)))
-                                    .build()))
-                        .keyboardRow(
-                            Collections.singletonList(
-                                InlineKeyboardButton.builder()
-                                    .text("Simple Alert")
-                                    .callbackData(
-                                        StringUtil.serialize(new AlertDto("Alert", false, null)))
-                                    .build()))
-                        .keyboardRow(
-                            Collections.singletonList(
-                                InlineKeyboardButton.builder()
-                                    .text("Simple Alert cached")
-                                    .callbackData(
-                                        StringUtil.serialize(
-                                            new AlertDto(
-                                                "Alert " + (new Random().nextInt() % 100),
-                                                false,
-                                                10)))
-                                    .build()))
-                        .build())
-                .build()));
+    List<Pidor> pidors =
+        pidorRepository.getAll().stream()
+            .filter(p -> p.getTgId() == 419320050)
+            .collect(Collectors.toList());
+    for (Pidor pidor : pidors) {
+      pidor.setFullName(pidor.getFullName().replaceAll("\uD83C\uDDE7\uD83C\uDDFE", "\uD83D\uDCA9"));
+      pidor.setUsername(pidor.getUsername().replaceAll("\uD83C\uDDE7\uD83C\uDDFE", "\uD83D\uDCA9"));
+      pidorRepository.update(pidor);
+    }
+    botActionCollector.text(message.getChatId(), new SimpleText("Done!"));
   }
 
   @Override
