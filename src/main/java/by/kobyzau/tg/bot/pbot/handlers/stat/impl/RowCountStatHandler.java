208047445c8b217ac4dd134fb7557d5861b51f2f
Mikita Kobyzau <Mikita_Kobyzau@epam.com>
2021-09-07 13:01:31 +0300

    11.4.6 Hot Potato Updated RowCountStatHandler

--- src/main/java/by/kobyzau/tg/bot/pbot/handlers/stat/impl/RowCountStatHandler.java
+++ src/main/java/by/kobyzau/tg/bot/pbot/handlers/stat/impl/RowCountStatHandler.java
@@ -10,7 +10,9 @@ import by.kobyzau.tg.bot.pbot.repository.CrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class RowCountStatHandler implements StatHandler {
@@ -21,15 +23,20 @@ public class RowCountStatHandler implements StatHandler {
   @Override
   public void printStat(long chatId) {
     int totalCount = 0;
+    Map<String, Integer> data = new HashMap<>();
     for (CrudRepository<?> repository : repositories) {
       int size = repository.getAll().size();
       totalCount += size;
-      botActionCollector.text(
-          chatId,
-          new ParametizedText(
-              "{0}: {1}",
-              new SimpleText(repository.getClass().getSimpleName()), new IntText(size)));
+      data.put(repository.getClass().getSimpleName(), size);
     }
+    data.entrySet().stream()
+        .sorted(Map.Entry.comparingByValue())
+        .forEach(
+            d ->
+                botActionCollector.text(
+                    chatId,
+                    new ParametizedText(
+                        "{0}: {1}", new SimpleText(d.getKey()), new IntText(d.getValue()))));
     botActionCollector.text(chatId, new ParametizedText("Total: {0}", new IntText(totalCount)));
   }
 
