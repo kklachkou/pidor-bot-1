d529a5e4e669c8d53fd44fe535211d960bb3d1c0
Mikita Kobyzau <Mikita_Kobyzau@epam.com>
2021-09-09 16:50:50 +0300

    11.7.1 Hot Potato Permission Checker

--- src/main/java/by/kobyzau/tg/bot/pbot/tg/action/SendAnimationBotAction.java
+++ src/main/java/by/kobyzau/tg/bot/pbot/tg/action/SendAnimationBotAction.java
@@ -2,9 +2,14 @@ package by.kobyzau.tg.bot.pbot.tg.action;
 
 import by.kobyzau.tg.bot.pbot.bots.Bot;
 import by.kobyzau.tg.bot.pbot.util.DateUtil;
+import by.kobyzau.tg.bot.pbot.util.TGUtil;
+import org.telegram.telegrambots.meta.api.methods.GetMe;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChatMember;
 import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMember;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class SendAnimationBotAction implements BotAction<Message> {
@@ -19,13 +24,20 @@ public class SendAnimationBotAction implements BotAction<Message> {
 
   @Override
   public Message process(Bot bot) throws TelegramApiException {
-    SendAnimation sendAnimation =
-        SendAnimation.builder()
-            .chatId(String.valueOf(chatId))
-            .animation(new InputFile(docId))
-            .disableNotification(DateUtil.sleepTime())
-            .build();
-    return bot.execute(sendAnimation);
+    User botUser = bot.execute(GetMe.builder().build());
+    ChatMember botMember =
+        bot.execute(
+            GetChatMember.builder().chatId(String.valueOf(chatId)).userId(botUser.getId()).build());
+    if (TGUtil.canSendOtherMessages(botMember)) {
+      SendAnimation sendAnimation =
+          SendAnimation.builder()
+              .chatId(String.valueOf(chatId))
+              .animation(new InputFile(docId))
+              .disableNotification(DateUtil.sleepTime())
+              .build();
+      return bot.execute(sendAnimation);
+    }
+    return null;
   }
 
   @Override
