7c5fdf085088095faca1dc46a9d772aa6aa22bfd
Mikita Kobyzau <64311392+kobyzau@users.noreply.github.com>
2021-09-25 11:54:42 +0300

    12.2.3 Artefacts
    Fix hot potato issue

--- src/main/java/by/kobyzau/tg/bot/pbot/service/impl/HotPotatoesServiceImpl.java
+++ src/main/java/by/kobyzau/tg/bot/pbot/service/impl/HotPotatoesServiceImpl.java
@@ -38,14 +38,14 @@ public class HotPotatoesServiceImpl implements HotPotatoesService {
   @Override
   public Optional<Pidor> getLastTaker(LocalDate localDate, long chatId) {
     return potatoTakerRepository.getByChatAndDate(chatId, localDate).stream()
-        .max(Comparator.comparing(HotPotatoTaker::getDeadline).thenComparing(HotPotatoTaker::getId))
+        .max(Comparator.comparing(HotPotatoTaker::getId))
         .flatMap(t -> pidorService.getPidor(t.getChatId(), t.getPlayerTgId()));
   }
 
   @Override
   public Optional<LocalDateTime> getLastTakerDeadline(LocalDate localDate, long chatId) {
     return potatoTakerRepository.getByChatAndDate(chatId, localDate).stream()
-        .max(Comparator.comparing(HotPotatoTaker::getDeadline).thenComparing(HotPotatoTaker::getId))
+        .max(Comparator.comparing(HotPotatoTaker::getId))
         .map(HotPotatoTaker::getDeadline);
   }
 
